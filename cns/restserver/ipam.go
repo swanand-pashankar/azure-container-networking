ad411144af9316a13c5192853b8b7f162b0eed27
Evan Baker <rbtr@users.noreply.github.com>
2021-06-09 17:36:30 -0500

    getPodIPIDByOrchestratorContexthandler

--- cns/restserver/ipam.go
+++ cns/restserver/ipam.go
@@ -192,34 +192,12 @@ func (service *HTTPRestService) GetPodIPConfigState() map[string]cns.IPConfigura
 }
 
 func (service *HTTPRestService) getPodIPIDByOrchestratorContexthandler(w http.ResponseWriter, r *http.Request) {
-	var (
-		resp          cns.GetPodContextResponse
-		statusCode    int
-		returnMessage string
-		err           error
-	)
-
-	statusCode = UnexpectedError
-
-	defer func() {
-		if err != nil {
-			resp.Response.ReturnCode = statusCode
-			resp.Response.Message = returnMessage
-		}
-
-		err = service.Listener.Encode(w, &resp)
-		logger.Response(service.Name, resp, resp.Response.ReturnCode, ReturnCodeToString(resp.Response.ReturnCode), err)
-	}()
-
-	resp.PodContext = service.GetPodIPIDByOrchestratorContext()
-
-	return
-}
-
-func (service *HTTPRestService) GetPodIPIDByOrchestratorContext() map[string]string {
 	service.RLock()
 	defer service.RUnlock()
-	return service.PodIPIDByOrchestratorContext
+	var resp cns.GetPodContextResponse
+	resp.PodContext = service.PodIPIDByOrchestratorContext
+	err := service.Listener.Encode(w, &resp)
+	logger.Response(service.Name, resp, resp.Response.ReturnCode, ReturnCodeToString(resp.Response.ReturnCode), err)
 }
 
 func (service *HTTPRestService) GetHTTPRestDataHandler(w http.ResponseWriter, r *http.Request) {
